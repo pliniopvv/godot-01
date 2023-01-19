6fc4099c2685141ed7de4c908141eada4c5f7826
Plinio Victor <gangss2@hotmail.com>
2023-01-18 22:46:54 -0300

    mapa modificado para adicionar colisões

--- scenes/player_black.gd
+++ scenes/player_black.gd
@@ -1,17 +1,19 @@
 extends KinematicBody2D
 
-var velocity := 16
+var velocity: Vector2
+
+export(int) var speed
 
 func _ready():
 	pass # Replace with function body.
 
 
 func _process(delta):
-	check_sprite_dir()
-	check_mov_player()
+	animate()
+	move()
 
 
-func check_sprite_dir():
+func animate():
 	if Input.is_action_just_pressed("ui_up"):
 		$anim.play("Idle_up")
 	if Input.is_action_just_pressed("ui_down"):
@@ -21,17 +23,11 @@ func check_sprite_dir():
 	if Input.is_action_just_pressed("ui_right"):
 		$anim.play("Idle_right")
 
-func check_mov_player():
-	if Input.is_action_just_pressed("ui_up"):
-		if ($".".position.y >= 32):
-			$".".position.y -= 16
-	if Input.is_action_just_pressed("ui_down"):
-		if ($".".position.y <= 16*38):
-			$".".position.y += 16
-	if Input.is_action_just_pressed("ui_left"):
-		if ($".".position.x >= 32):
-			$".".position.x -= 16
-	if Input.is_action_just_pressed("ui_right"):
-		if ($".".position.x <= 16*48):
-			$".".position.x += 16
-	pass
+func move():
+	var direction_vector: Vector2 = Vector2(
+		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
+		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	).normalized()
+	
+	velocity = direction_vector * speed
+	velocity = move_and_slide(velocity)
